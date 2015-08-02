0906a363e4fb7017bd422a73cc8a53d96a394499
Adam Bell <agbell@gmail.com>
2015-08-02 15:29:01 -0400

    SAVEPOINT

--- app/Main.hs
+++ app/Main.hs
@@ -21,13 +21,26 @@ main =
       compile $ pandocCompiler
         >>= loadAndApplyTemplate "templates/default.html" defaultContext
         >>= relativizeUrls
-
+    -- build up tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged \"" ++ tag ++ "\""
+        route idRoute
+        compile $ do
+          posts <- recentFirst =<< loadAll pattern
+          let ctx = constField "title" title
+                    `mappend` listField "posts" postContext (return posts)
+                    `mappend` defaultContext
+          makeItem ""
+                    >>= loadAndApplyTemplate "templates/tag.html" ctx
+                    >>= loadAndApplyTemplate "templates/default.html" ctx
+                    >>= relativizeUrls
     match "posts/*" $ do
       route (setExtension "html")
       compile $ pandocCompiler
-        >>= loadAndApplyTemplate "templates/post.html" postContext
+        >>= loadAndApplyTemplate "templates/post.html" (postContextWithTags tags)
         >>= saveSnapshot "content"
-        >>= loadAndApplyTemplate "templates/default.html" postContext
+        >>= loadAndApplyTemplate "templates/default.html" (postContextWithTags tags)
         >>= relativizeUrls
 
     create ["archive.html"] $ do
@@ -81,6 +94,9 @@ feedContext =
 postContext :: Context String
 postContext =
   dateField "date" "%Y-%m-%d" `mappend` defaultContext
+
+postContextWithTags :: Tags -> Context String
+postContextWithTags tags = tagsField "tags" tags `mappend` postContext
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration =
